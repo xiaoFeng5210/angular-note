0a23ddf3c6f578997ec77c100a326ac471984dbd
xiaoFeng5210 <38905126+xiaoFeng5210@users.noreply.github.com>
2020-08-24 23:56:24 +0800

    Update hero-detail.component.ts
    响应式表单验证

--- ng-demo/src/app/hero-detail/hero-detail.component.ts
+++ ng-demo/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import {FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
 
 import { Hero } from '../hero';
 import { HeroService }  from '../hero.service';
@@ -12,6 +13,19 @@ import { HeroService }  from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
+  profileForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    lastName: new FormControl(''),
+    choose: new FormControl(''),
+    address: new FormGroup({
+      xuzhou: new FormControl(''),
+      nanjing: new FormControl('')
+    })
+  });
+  get firstName() { return this.profileForm.get('firstName') }
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -21,14 +35,20 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroDetail();
   }
+  onSubmit() {
+    console.log(this.profileForm.value)
+  }
+  changeChoose() {
+    this.profileForm.patchValue({
+      choose: true
+    })
+  }
   getHeroDetail() {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
-  }
-  save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+    this.heroService.testObservable().subscribe(
+      x => console.log(x))
   }
   goBack() {
     this.location.back();
